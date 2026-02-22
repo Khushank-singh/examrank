d383cd4f4483d06528d543016177e17a046a2a63
khushank-singh <khushanksingh31@gmail.com>
2026-02-22 19:06:10 +0530

    Fixed CORS for production

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,11 @@ const authMiddleware = require("./authMiddleware");
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true
+}));
 app.use(express.json());
 
 
